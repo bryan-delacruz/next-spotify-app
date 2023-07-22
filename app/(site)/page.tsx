e8047b3bb688b12a1ba6e7893722ecb4d1f5a9d6
Bryan De La Cruz <bryan.delacruza@gmail.com>
2023-07-22 11:07:12 -0500

    Layout-In Progress

---
+++ app/(site)/page.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+export default function Home() {
+  return (
+    <div
+      className="
+    bg-neutral-900
+    rounded-lg
+    h-full
+    w-full
+    overflow-hidden
+    overflow-y-auto"
+    >
+      <Header><div>Header</div></Header>
+    </div>
+  );
+}
