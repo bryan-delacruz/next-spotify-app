19b0191debb7b4a333e965ecb8743056cbe7d6df
Bryan De La Cruz <bryan.delacruza@gmail.com>
2023-07-26 19:50:51 -0500

    Supabse Types-Added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 export default function Home() {
   return (
     <div
@@ -10,7 +11,26 @@ export default function Home() {
     overflow-hidden
     overflow-y-auto"
     >
-      <Header><div>Header</div></Header>
+      <Header>
+        <div>
+          <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
+          <div className="grid grid-cols-1 sm:grid-flow-col-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
+          </div>
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
     </div>
   );
 }
