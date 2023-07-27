540bb99ba5eef39061a48550a250662d5c27cbbd
Bryan De La Cruz <bryan.delacruza@gmail.com>
2023-07-27 00:00:45 -0500

    Providers for auth and supabase-Done

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -3,7 +3,7 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect, useContext } from "react";
 
 import { Subscription, UserDetails } from "@/types";
 
@@ -42,4 +42,48 @@ export const MyUserContextProvider = (props: Props) => {
       .select("*,prices(*,products(*))")
       .in("status", ["trialing", "active"])
       .single();
+
+  useEffect(() => {
+    if (user && !isLoadingData && !userDetails && !subscription) {
+      setIsLoadingData(true);
+      Promise.allSettled([getUserDetails(), getSubscription()]).then(
+        (results) => {
+          const userDetailsPromise = results[0];
+          const subscriptionPromise = results[1];
+
+          if (userDetailsPromise.status === "fulfilled") {
+            setUserDetails(userDetailsPromise.value.data as UserDetails);
+          }
+
+          if (subscriptionPromise.status === "fulfilled") {
+            setSubscription(subscriptionPromise.value.data as Subscription);
+          }
+
+          setIsLoadingData(false);
+        }
+      );
+    } else if (!user && !isLoadingUser && isLoadingData) {
+      setUserDetails(null);
+      setSubscription(null);
+    }
+  }, [user, isLoadingUser]);
+
+  const value = {
+    accessToken,
+    user,
+    userDetails,
+    isLoading: isLoadingUser || isLoadingData,
+    subscription,
+  };
+
+  return <UserContext.Provider value={value} {...props} />;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a MyUserContextProvider");
+  }
+
+  return context
 };
