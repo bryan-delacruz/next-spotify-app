062989fe753bcada1974441317c45ab3ca54adfd
Bryan De La Cruz <bryan.delacruza@gmail.com>
2023-09-18 20:16:09 -0500

    Favorite Functionality Added

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -9,6 +9,8 @@ interface SearchProps {
   };
 }
 
+export const revalidate = 0;
+
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
 
@@ -25,8 +27,8 @@ const Search = async ({ searchParams }: SearchProps) => {
       <Header className="from-bg-neutral-900">
         <div className="mb-2 flex flex-col gap-y-6">
           <h1 className="text-white text-3xl font-semibold">Search</h1>
-          <SearchInput/>
-          <SearchContent songs={songs}/>
+          <SearchInput />
+          <SearchContent songs={songs} />
         </div>
       </Header>
     </div>
