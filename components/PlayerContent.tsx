2cf5ad846da78d1803a35acd954361a1d5968cd5
Bryan De La Cruz <bryan.delacruza@gmail.com>
2023-09-29 13:17:06 -0500

    Player Content-onClick event on Play Btn added for mobile

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -93,7 +93,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
       </div>
       <div className="flex md:hidden col-auto w-full justify-end items-center">
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon size={30} className="text-black" />
